b11f106db38e806e9c7a78a380914c2102b7f798
gmcnutt <gmcnutt@cableone.net>
2019-12-19 18:04:59 -0700

    Fix ledge-walking

--- demo.c
+++ demo.c
@@ -608,7 +608,7 @@ bool move_cursor(area_t *area, view_t * view, const point_t dir)
                 } else {
                         /* Else if it's passable */
                         if (!PIXEL_IS_IMPASSABLE(pix)) {
-                                int lvl_z = L2Z(Z2L(view->cursor[Z]));
+                                int lvl_z = L2Z(Z2L(newcur[Z]));
                                 int new_z =  (PIXEL_HEIGHT(pix) + lvl_z);
                                 if (!(map = area_get_map_at_level(area, Z2L(new_z))) ||
                                     !map_contains(map, newcur[X], newcur[Y])) {
